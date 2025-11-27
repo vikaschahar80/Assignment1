fa09dc328efe3ec232e28a9d653a103ddc321812
Vikash Chahar <vikas@Vikashs-MacBook-Air.local>
2025-11-27 16:15:55 +0530

    Flatten folder structure

--- server/app.ts
+++ server/app.ts
@@ -85,7 +85,7 @@ export default async function runApp(
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = parseInt(process.env.PORT || '5001', 10);
   const listenOptions = {
     port,
     host: "0.0.0.0",
